927757cab6c3f8ba9a956269f02b65c8227dd438
MartijnRikken <60739643+MartijnRikken@users.noreply.github.com>
2023-03-13 15:42:16 +0100

    Martijn

--- src/main/java/com/github/hanyaeger/tutorial/scenes/GameLevel.java
+++ src/main/java/com/github/hanyaeger/tutorial/scenes/GameLevel.java
@@ -1,6 +1,15 @@
 package com.github.hanyaeger.tutorial.scenes;
 
+import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.scenes.DynamicScene;
+import com.github.hanyaeger.api.userinput.KeyListener;
+import com.github.hanyaeger.api.userinput.MouseButtonPressedListener;
+import com.github.hanyaeger.tutorial.entities.buttons.Henny;
+import com.github.hanyaeger.tutorial.entities.buttons.Swordfish;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+
+import java.util.Set;
 
 public class GameLevel extends DynamicScene {
     @Override
@@ -11,6 +20,9 @@ public class GameLevel extends DynamicScene {
 
     @Override
     public void setupEntities() {
-
+        var zwaard = new Swordfish(new Coordinate2D(1400, 200));
+        var henny = new Henny(new Coordinate2D(10, 10));
+        addEntity(zwaard);
+        addEntity(henny);
     }
 }
